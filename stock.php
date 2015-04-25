e54b3f6eabfb0e66291dbb3d97e20834a2e3f42e
zdliang <zdliang@yeah.net>
2015-04-25 22:02:41 +0800

    use parameter for stock number

--- stock.php
+++ stock.php
@@ -2,12 +2,14 @@
 // Connecting, selecting database
 //Database=yuanbao;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=b38b9a71f4f907;Password=5b3f6d06
 //$link = mysql_connect('127.0.0.1', 'admin', '1a2b3c') or die('Could not connect: ' . mysql_error());
+$stockNum = $_GET["stockNum"];
 $link = mysql_connect('ap-cdbr-azure-east-c.cloudapp.net', 'b38b9a71f4f907', '5b3f6d06') or die('Could not connect: ' . mysql_error());
 echo 'Connected successfully';
 mysql_select_db('yuanbao') or die('Could not select database');
 
 // Performing SQL query
-$query = "SELECT * FROM yuanbao.close_predict where symbol='000001'";
+$query = "SELECT * FROM yuanbao.close_predict where symbol='".$stockNum."'";
+//$query = "SELECT * FROM yuanbao.close_predict where symbol='600028'";
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 // Printing results in HTML
