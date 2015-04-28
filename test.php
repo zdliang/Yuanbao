3445cacd5b3f1b5f7cd4ad791b7107786580c193
zdliang <zdliang@yeah.net>
2015-04-28 23:15:53 +0800

    shoter

--- test.php
+++ test.php
@@ -11,12 +11,21 @@ else{
 	//echo sprintf("%.2f", $stockList[0]["meanPredictExcessRet"]*100)."%<BR />";
 	echo "未来20日预期绝对回报 ".sprintf("%.2f", $stockList[0]["meanPredictRet"]*100)."%  预期相对回报".sprintf("%.2f", $stockList[0]["meanPredictExcessRet"]*100)."%\n本数据由历史数据模拟而成，不作为投资依据，\n投资者据此操作，我公司不负任何责任";
 	echo "<BR />";
+	$content[] = array("Title"=>"股票代码：".$keyword, 
+                                   "Description"=>"未来20日预期绝对回报 ".sprintf("%.2f", $stockList[0]["meanPredictRet"]*100)."%  预期相对回报".sprintf("%.2f", $stockList[0]["meanPredictExcessRet"]*100)."%\n本数据由历史数据模拟而成，不作为投资依据，\n投资者据此操作，我公司不负任何责任", 
+                                   "PicUrl"=>"", 
+                                   "Url" =>"");
 	$index = 1;
 	foreach ($stockList as $stock) {		
 		//echo $index."<BR />";
 		//echo "历史最像走势第".$index."名 ".$stock["matchedSymbol"]." ".date("Y/m/d",strtotime($stock["matchedWinStartDate"]))."-".date("Y/m/d",strtotime($stock["matchedWinEndDate"]))."<BR />";
 		echo "历史最像走势第".$index."名 ".$stock["matchedSymbol"]." ".date("Y/m/d",strtotime($stock["matchedWinStartDate"]))."-".date("Y/m/d",strtotime($stock["matchedWinEndDate"]));
 		echo "<BR />";
+		$content[] = array(//"Title"=>"历史最像走势第".$index."名 ".$stock["matchedSymbol"]." ".date("Y/m/d",strtotime($stock["matchedWinStartDate"]))."-".date("Y/m/d",strtotime($stock["matchedWinEndDate"])), 
+                        "Title"=>$index." ".$stock["matchedSymbol"]." ".date("Y/m/d",strtotime($stock["matchedWinStartDate"]))."-".date("Y/m/d",strtotime($stock["matchedWinEndDate"])), 
+                        "Description"=>"", 
+                        "PicUrl"=>"", 
+                        "Url" =>"");
 		//echo sprintf("%.2f", $stock["meanPredictExcessRet"]*100)."%<BR />";
 		$index++;
 		// foreach ($stock as $key => $value) {
